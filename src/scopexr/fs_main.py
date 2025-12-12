ab54c5ab735c7d6f7f1809accd1dc5c30f66ac6f
jacopoaltieri <jacopo.altieri@gmail.com>
2025-12-12 15:21:51 +0100

    separated runners to improve readability

--- src/scopexr/fs_main.py
+++ src/scopexr/fs_main.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from .runners import run_pipeline_fs
+from .fs_runner import run_pipeline_fs
 
 
 def main():
